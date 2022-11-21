a4291d71e518263c051069709725a5f808396d8f
isaacbrds <isaac.brigido@gmail.com>
2022-11-21 11:29:27 -0300

    test: adiciona caso de teste ao metodo sort

--- spec/bubble_sort_spec.rb
+++ spec/bubble_sort_spec.rb
@@ -4,11 +4,7 @@ RSpec.describe Algorithms::BubbleSort do
   end
 
 
-  #it "should return the sorted list" do
-  # expect(Algorithms::BubbleSort.sort_list(:arr)).eql?([1,2,3,4,5])
-  #end
-
-  it 'should return voltando' do
-    expect(Algorithms::BubbleSort.volta()).eql?('Voltando')
+  it "should return the sorted list" do
+   expect(Algorithms::BubbleSort.sort_list(:arr)).eql?([1,2,3,4,5])
   end
 end
