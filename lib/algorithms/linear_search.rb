1992616c45306b866c860d3daf605fd63fe1ee35
isaacbrds <isaac.brigido@gmail.com>
2022-11-22 06:36:00 -0300

    refactor: Adiciona lógica ao método

--- lib/algorithms/linear_search.rb
+++ lib/algorithms/linear_search.rb
@@ -1,7 +1,11 @@
 module Algorithms
   class LinearSearch
     def self.find(arr, choosed)
-      choosed
+      for elemento in arr do
+        if elemento == choosed
+          return elemento
+        end
+      end
     end
   end
 end
