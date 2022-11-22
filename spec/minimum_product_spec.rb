97da0ddfb0229ea61013dd980ffcebccc9d79cae
isaacbrds <isaac.brigido@gmail.com>
2022-11-22 10:15:39 -0300

    test: testes adicionados

--- spec/minimum_product_spec.rb
+++ spec/minimum_product_spec.rb
@@ -1,8 +1,18 @@
 RSpec.describe Algorithms::MinimumProduct do
   describe 'Calculate the minimum product of list' do
     let(:list) { [-1, 0] }
-    it 'should be 1' do
+    it 'has a zero' do
       expect(Algorithms::MinimumProduct.calculate(list)).eql?(-1)
+    end
+
+    let(:new_list) { [-1, -1, -2, 4, 3] }
+    it 'negative is different positive' do
+      expect(Algorithms::MinimumProduct.calculate(new_list)).eql?(-24)
+    end
+
+    let(:new_list) { [-1, -2, 4, 3] }
+    it 'negative is equal positive' do
+      expect(Algorithms::MinimumProduct.calculate(new_list)).eql?(-4)
     end
   end
 end
